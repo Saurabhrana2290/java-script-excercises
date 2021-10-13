48f3bca4f897978fb8d8d1ba5d79b7cd16c2c4d1
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-10-13 15:06:58 +0530

    Create- ex-4 created

---
+++ ex-4-99-beers-of-bottles.js
@@ -0,0 +1,9 @@
+var count =99;
+
+  function beers(){
+    while(count >= 0){
+    console.log(`${count} beers of bottles on the wall, ${count} bottles of beers.\nTake one down and pass it around, ${count-1}  beers of bottles on the wall`);
+    count--;
+  }
+   
+}
