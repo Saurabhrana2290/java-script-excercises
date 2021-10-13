fa0a8575346bebc3ffcde13d78c822858a300b72
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-10-13 14:56:07 +0530

    Update- file beutified

--- ex-2.js
+++ ex-2.js
@@ -1,26 +1,26 @@
 var output = [];
-  var i = 1;
-function fizzBuzz(){
-    while(i<=20){
-    if(i % 3 === 0){
-        if(i % 5 === 0){
-            output.push("Fizzbuzz");
+var i = 1;
+function fizzBuzz() {
+    while (i <= 20) {
+        if (i % 3 === 0) {
+            if (i % 5 === 0) {
+                output.push("Fizzbuzz");
+            }
+            else {
+                output.push("Fizz");
+            }
         }
-        else{
-            output.push("Fizz");   
+        else if (i % 5 === 0) {
+            if (i % 3 === 0) {
+                output.push("Fizzbuzz");
+            }
+            else {
+                output.push("Buzz");
+            }
         }
-    } 
-    else if(i % 5 === 0){
-         if(i % 3 === 0){
-            output.push("Fizzbuzz");
+        else {
+            output.push(i);
         }
-        else{
-            output.push("Buzz");   
-        }
-    }
-    else{
-    output.push(i);
-    }
     }
     i++;
     console.log(output);
